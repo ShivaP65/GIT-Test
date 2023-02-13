2d911c585aa04b8e2daab26f6185d7a01a11ae1a
Shiva pandiri <shivap65@gmail.com>
2023-02-13 19:36:14 +0530

    Changes done

--- Helo.py
+++ Helo.py
@@ -3,6 +3,6 @@ print('Hello*****2')
 print('Hello*****3')
 print('*****-Testing file to get from GIT')
 
-f = open("Data writing to File", "a")
+f = open("C:/Shiva/exe.txt", "a")
 f.write("Now the file has more content!")
 f.close()
