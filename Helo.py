f01318dece837bc40d80ef96fd0ed18c843561d6
Shiva pandiri <shivap65@gmail.com>
2023-02-13 19:30:20 +0530

    File Writing changes

--- Helo.py
+++ Helo.py
@@ -2,3 +2,7 @@ print('Hello*****')
 print('Hello*****2')
 print('Hello*****3')
 print('*****-Testing file to get from GIT')
+
+f = open("Data writing to File", "a")
+f.write("Now the file has more content!")
+f.close()
